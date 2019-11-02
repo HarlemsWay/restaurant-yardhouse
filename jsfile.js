80dac61c11e4a6e0b0540e8a5191611ae80687b1
Brandon (BG) Gabbard <bmgabbard0523@gmail.com>
2019-11-02 18:48:11 -0400

    popup window caret down/up function

--- jsfile.js
+++ jsfile.js
@@ -2,6 +2,8 @@
 function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
+    var popup = document.getElementById("caret").innerHTML="&#xf0d8";
+    
 }
 
 //Question:  I don't have a .show class in my index.  I do have one in 
