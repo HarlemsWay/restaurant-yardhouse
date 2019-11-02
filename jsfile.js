412405f1a400deef900d0a1b96f77ec3f1eb0ea7
Brandon (BG) Gabbard <bmgabbard0523@gmail.com>
2019-11-02 17:52:16 -0400

    New loc(location) button style and hover

--- jsfile.js
+++ jsfile.js
@@ -4,4 +4,8 @@ function myFunction() {
     popup.classList.toggle("show");
 }
 
+//Question:  I don't have a .show class in my index.  I do have one in 
+//css and javascript.  Does/how does .show connect css and js, wo index class
+
+
 //footer join button
