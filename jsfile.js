5563a63c4d019e613c4d7a3320c5e5ec719773f5
Brandon (BG) Gabbard <bmgabbard0523@gmail.com>
2019-11-02 21:08:24 -0400

    onclick caret up and down in nav

--- jsfile.js
+++ jsfile.js
@@ -3,8 +3,18 @@ function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
     var popup = document.getElementById("caret").innerHTML="&#xf0d8";
-    
+    //work on making caret point up and down on each click
+    // if (popup = ("show")) {
+         //   fa.innerHTML = "&#xf0d8";
+       // } else {
+         //   fa.innerHTML = "&#xf0d7";
+        //}
+
 }
+
+
+//caret up and down
+
 
 //Question:  I don't have a .show class in my index.  I do have one in 
 //css and javascript.  Does/how does .show connect css and js, wo index class
