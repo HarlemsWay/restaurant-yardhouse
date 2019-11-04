949fc5f710b6b22a89140c045cc6e85c0e5bc1b9
Brandon (BG) Gabbard <bmgabbard0523@gmail.com>
2019-11-03 20:17:15 -0500

    footer date of birth dropdown. still needs work

--- jsfile.js
+++ jsfile.js
@@ -3,21 +3,26 @@ function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
     var popup = document.getElementById("caret").innerHTML="&#xf0d8";
-    //work on making caret point up and down on each click
-    // if (popup = ("show")) {
-         //   fa.innerHTML = "&#xf0d8";
-       // } else {
-         //   fa.innerHTML = "&#xf0d7";
-        //}
-
+     
 }
-
+//working on footer dropdown and nav caret(forget about that for now)
 
 //caret up and down
 
 
 //Question:  I don't have a .show class in my index.  I do have one in 
 //css and javascript.  Does/how does .show connect css and js, wo index class
+
+//footer date of birth dropdown---needs coded to html elements
+$(document).ready(function(){
+  $(".dropdown").on("hide.bs.dropdown", function(){
+    $(".btn").html(' <span class="caret"></span>');
+  });
+  $(".dropdown").on("show.bs.dropdown", function(){
+    $(".btn").html(' <span class="caret caret-up"></span>');
+  });
+});
+
 
 
 //footer join button
